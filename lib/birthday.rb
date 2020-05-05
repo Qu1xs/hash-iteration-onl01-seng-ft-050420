0ebc5d07a2111d8096539c80e0eaeebb857249ab
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 01:58:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,9 @@
 
 def happy_birthday(birthday_kids)
   happy_birthday(birthday_kids)
-    birthday_kids.each do
-      |kids_name, age|
-  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
